c527c986c2b91bd1e81b60c365fc86d3a4e002f6
Aishwarya <aishwarya.gaikwad@soft-corner.com>
2019-09-10 18:14:51 +0530

    url change for add location

--- code_base/survey_form/urls.py
+++ code_base/survey_form/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('connect-kobo/', views.connect_kobo, name='connect'),
+    # path('connect-kobo/', views.connect_kobo, name='connect'),
 
 ]
