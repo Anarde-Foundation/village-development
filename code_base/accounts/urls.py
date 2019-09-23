6d31a92138d65bc3b033e46d5ec53da975291bef
Aishwarya <aishwarya.gaikwad@soft-corner.com>
2019-09-23 14:46:53 +0530

    template name change for account

--- code_base/accounts/urls.py
+++ code_base/accounts/urls.py
@@ -8,9 +8,9 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = [
 
     path('register/',views.register_view, name='register'),
-    path('user_new/',views.add_user, name='user_add'),
-    path('user_update/<int:pk>',views.update_view, name='user_update'),
-    path('user_list', views.user_list.as_view(), name='user_list'),
+    path('account_create/',views.account_create, name='account_create'),
+    path('account_update/<int:pk>',views.account_update, name='account_update'),
+    path('account_list', views.account_list.as_view(), name='account_list'),
     path('get_user_list_for_datatable',views.get_user_list_for_datatable, name='get_user_list_for_datatable'),
 
 
