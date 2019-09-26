bd210d39bebc21d274b8f33e6cd2b759e86fdc3e
Omkar <omkar.ghanekar@soft-corner.com>
2019-09-26 16:08:46 +0530

    added kobo pull form data

--- code_base/utils/configuration.py
+++ code_base/utils/configuration.py
@@ -1,8 +1,9 @@
-
 class kobo_constants:
     user_name = "<kobo account user name>"
     pwd = "<kobo account password>"
 
     authorization_token = "<kobo auth Token >"
 
-    form_info_link = "<kobo form metadata url>"
+    kobo_form_link = "<kobo form metadata url>"
+    #kobo_data_link = "http://kc.kobo.local/api/v1/data/"
+    kobo_data_link = "<kobo form data url>"
---
+++ code_base/utils/constants.py
@@ -0,0 +1,6 @@
+class kobo_form_constants:
+
+    data_format = "/form.json"
+    question_type = ['text', 'calculate', 'integer']
+    question_type_having_options = ['select one', 'select all that apply']
+    names_not_allowed = ['start', 'end', 'meta', '__version__', 'instanceID']
