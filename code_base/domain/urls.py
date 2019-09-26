6a4ab5e9ff243550e713e5d5c97b612122a61ec4
Aishwarya <aishwarya.gaikwad@soft-corner.com>
2019-09-26 12:04:42 +0530

    domain list, create update

--- code_base/domain/urls.py
+++ code_base/domain/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('domain_create', views.domain_create, name='domain_create'),
     path('view/<int:pk>', views.domain_view, name='domain_view'),
     path('edit/<int:pk>', views.domain_update, name='domain_update'),
+    path('delete/<int:pk>', views.domain_delete, name='domain_delete'),
+
 
     path('json', views.get_domain_list_for_datatable,
          name='get_domain_list_for_datatable'),
--- code_base/domain/views.py
+++ code_base/domain/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+from django.template.loader import render_to_string
 from django.contrib.auth.decorators import login_required
 from domain.models import domain
 from django import forms
 from django.forms import ModelForm
 from django.http import HttpResponse
 from django.core import serializers
+from django.core.validators import RegexValidator
+import json
 # Create your views here.
 
 
 class DomainForm(ModelForm):
+    validator = RegexValidator(
+    r'^[a-zA-Z0-9_]*$',
+    message = ("No spaces allowed. use '_'"),
+    code='invalid_group_key')
 
+    kobo_group_key = forms.CharField(validators = [validator], required=True)
     class Meta:
         model = domain
-        fields = ['domain_name', 'description']
+        fields = ['domain_name', 'kobo_group_key', 'description']
 
 
 @login_required
@@ -67,3 +76,15 @@ def domain_update(request, pk, template_name='domain_update.html'):
             print(form.errors)
 
     return render(request, template_name, {'form':form})
+
+@login_required
+def domain_delete(request, pk):
+    domainForDelete = get_object_or_404(domain, pk=pk)
+    domainForDelete.delete()
+    return redirect('/domain/')
+
+def delete(request):
+    candidate = Candidates.objects.get(pk = int(request.REQUEST['id']))
+    candidate.delete()
+    payload = {'success': True}
+    return HttpResponse(json.dumps(payload), content_type='application/json')
