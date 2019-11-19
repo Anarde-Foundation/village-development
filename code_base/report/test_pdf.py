896baaac1f18282fe27a13c3a2a966c016f3bb3c
Aishwarya <aishwarya.gaikwad@soft-corner.com>
2019-11-19 17:25:53 +0530

    pdf using weasyprint

--- code_base/report/test_pdf.py
+++ code_base/report/test_pdf.py
@@ -1,4 +1,5 @@
 from io import BytesIO
+from reportlab.pdfgen import canvas
 from xhtml2pdf import pisa
 from django.template.loader import get_template
 from django.template import Context
@@ -8,20 +9,45 @@ from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter, inch
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 
+
 def render_to_pdf(template_src, context_dict):
     template = get_template(template_src)
     context = context_dict
     html  = template.render(context)
     result = BytesIO()
-    # data = list(context_dict)
-    # t = Table(data, 5 * [0.4 * inch], 4 * [0.4 * inch])
-    # t.setStyle(TableStyle([('ALIGN', (1, 1), (-2, -2), 'RIGHT'), ('TEXTCOLOR', (1, 1), (-2, -2), colors.red),
-    #                        ('VALIGN', (0, 0), (0, -1), 'TOP'), ('TEXTCOLOR', (0, 0), (0, -1), colors.blue),
-    #                        ('ALIGN', (0, -1), (-1, -1), 'CENTER'), ('VALIGN', (0, -1), (-1, -1), 'MIDDLE'),
-    #                        ('TEXTCOLOR', (0, -1), (-1, -1), colors.green),
-    #                        ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
-    #                        ('BOX', (0, 0), (-1, -1), 0.25, colors.black), ]))
+
     pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
     if not pdf.err:
+        # Create a canvas and add a rectangle to it
+        c = canvas.Canvas(result)
+        c.translate(inch, 9 * inch)
+        c.rect(inch, inch, 1 * inch, 1 * inch, fill=1)
+
+        # example.xlsx is in the same directory as the pdf
+        c.linkURL(r'Metabase.html', (inch, inch, 2 * inch, 2 * inch), relative=1)
+        c.save()
         return HttpResponse(result.getvalue(), content_type='application/pdf')
+
     return HttpResponse('We had some errors<pre>%s</pre>' % escape(html))
+
+def write_pdf_view(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'inline; filename="mypdf.pdf"'
+
+    buffer = BytesIO()
+    p = canvas.Canvas(buffer)
+
+    # Start writing the PDF here
+    p.drawString(100, 100, 'Hello world.')
+    # End writing
+
+    p.showPage()
+    p.save()
+
+    pdf = buffer.getvalue()
+    buffer.close()
+    response.write(pdf)
+
+    return response
+
+
--- code_base/report/urls.py
+++ code_base/report/urls.py
@@ -1,10 +1,20 @@
 from django.conf.urls import url
 from django.urls import path, include
 from . import views
+from wkhtmltopdf.views import PDFTemplateView
 
 
 app_name = 'report'
 urlpatterns = [
 path('pdf/<int:pk>', views.InvoicePDFView, name='pdf'),
+path('graph_pdf/<int:pk>/<int:domain_id>', views.domain_graphs_pdf, name='graph_pdf'),
+path('graph_pdf/<int:pk>/<int:domain_id>', views.domain_graphs_pdf, name='graph_pdf'),
 path('get_survey_question_list_for_pdf/<int:pk>', views.get_survey_question_list_for_pdf, name='get_survey_question_list_for_pdf'),
-    ]
+path('new', PDFTemplateView.as_view(template_name='Metabase.html',
+                                           filename='my_pdf.pdf'), name='pdf'),
+    path('weasy_pdf', views.pdf_generation, name='weasy_pdf'),
+    path('survey_pdf/<int:pk>', views.html_to_pdf_generation, name='survey_pdf'),
+    path('survey_html_page/<int:pk>', views.survey_html, name='survey_html_page'),
+    path('weasy_html_page', views.show_weasy_html_page, name='weasy_html_page'),
+    path('weasy_embed_svg', views.weasy_embed_svg, name='weasy_embed_svg'),
+]
