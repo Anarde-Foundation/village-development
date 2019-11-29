ca0ed2f47f2678b422a9f0c991dc54fe92d452df
Omkar <omkar.ghanekar@soft-corner.com>
2019-11-29 11:43:00 +0530

    report changes

--- code_base/report/urls.py
+++ code_base/report/urls.py
@@ -5,9 +5,6 @@ from . import views
 app_name = 'report'
 urlpatterns = [
 
-    path('weasy_pdf', views.pdf_generation, name='weasy_pdf'),
     path('survey_pdf/<int:pk>', views.html_to_pdf_generation, name='survey_pdf'),
     path('survey_html_page/<int:pk>', views.survey_html, name='survey_html_page'),
-    path('weasy_html_page', views.show_weasy_html_page, name='weasy_html_page'),
-    path('weasy_embed_svg', views.weasy_embed_svg, name='weasy_embed_svg'),
 ]
