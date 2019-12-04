72f6b49173412f3efc298a588f21e9384803f42a
Omkar <omkar.ghanekar@soft-corner.com>
2019-12-04 11:57:02 +0530

    added config file

---
+++ code_base/utils/configuration.py
@@ -0,0 +1,44 @@
+# from code_base.Anarde import settings
+import os
+
+
+class kobo_constants:
+    user_name = "<kobo_user name>"
+    pwd = "<kobo pwd>"
+
+    authorization_token = "<kobo authorization token>"
+
+    kobo_form_link = "<kobo form link>"
+    kobo_data_link = "<kobo data link>"
+
+
+class metabase_constants:
+
+    metabase_site_url = "<metabase site url>"
+    metabase_secret_key = "<metabase site secret key>"
+
+
+class image_constants:
+
+    is_production = False
+
+    localhost = "<machine ip>"
+    STATIC_URL = '/code_base/static/'
+    currentDir = os.path.normpath(os.getcwd() + os.sep + os.pardir)
+    before_afterDir = currentDir + STATIC_URL + r"before_after/"
+
+    before_afterDirStatic = "/static/before_after/"
+
+    image_type_before = "before"
+    image_type_after = "after"
+
+    image_dir = r"before_after/"
+
+
+class aws_bucket_constants:
+    s3_bucket_path = "<aws bucket path>"
+    aws_access_key_id = '<aws access key>'
+    aws_secret_access_key = '<aws secret key access>'
+    region_name = '<region name>'
+    bucket_name = '<bucket name>'
+
