4e8588e80fe92a5aead837ad098b9d58e938a7f2
Omkar <omkar.ghanekar@soft-corner.com>
2019-12-17 17:12:15 +0530

    added survey reports

--- code_base/report/urls.py
+++ code_base/report/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path, include
 from . import views
 
-
 app_name = 'report'
 urlpatterns = [
 
     path('survey_pdf/<int:pk>', views.html_to_pdf_generation, name='survey_pdf'),
     path('survey_html_page/<int:pk>', views.survey_html, name='survey_html_page'),
-    path('svg/', views.svg_convert, name='svgc'),
-    # path('svg/', views.get_svg, name='svgc'),
 ]
